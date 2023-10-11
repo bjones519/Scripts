6e183ec256323e2bdbdc2ada9eaa6fec7f9b58a3
Brittney Jones <87450561+bjones519@users.noreply.github.com>
2023-10-11 11:35:27 -0400

    Update VSCodeInstall.sh

--- VSCodeInstall.sh
+++ VSCodeInstall.sh
@@ -6,3 +6,5 @@ echo ‘’'bind-addr: 0.0.0.0:8081
 auth: password
 password: abcd1234
 cert: false’’' > ./.config/code-server/config.yaml
+sudo systemctl stop code-server@$USER
+sudo systemctl enable --now code-server@$USER
