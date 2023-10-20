1a88531b9eb8fa73df2496d815f84fdf0061b931
Brittney Jones <87450561+bjones519@users.noreply.github.com>
2023-10-20 11:10:28 -0400

    Update runningProcesses.py

--- runningProcesses.py
+++ runningProcesses.py
@@ -1,7 +1,7 @@
 import os
 import psutil
 import csv
-# Using the Python modules os, psutil, and CSV, 
+
 # create a CSV file that lists all running processes and has columns for process ID #, name, executable path, CPU usage, and mem usage. 
 
 
